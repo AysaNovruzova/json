5fd3e9c311ca4002a9b949cb7eb837dc3cc54651
Aysa <bd7hwd97w@code.edu.az>
2024-05-28 16:54:01 +0400

    task

---
+++ Json/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace Json
+{
+
+   public class Program
+    {
+        public static void Main()
+        {
+            Person person = new Person { Name = "John", Age = 30 };
+            string jsonString = JsonSerializer.Serialize(person);
+            Console.WriteLine(jsonString);
+
+             static void Main()
+            {
+                string jsonString = "{\"Name\":\"John\",\"Age\":30}";
+                Person person = JsonSerializer.Deserialize<Person>(jsonString);
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            }
+        }
+    }
+
+}
+
